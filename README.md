9dc2a3c073dfafbe17baf97b5e23838b4cfdf7dd
victorex27 <aobikobe@gmail.com>
2022-10-04 13:12:20 +0100

    add RobotBuilder Page

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# VueJsTutorial
-Vuejs Playbook on Pluralsight
+# vuejstutorial
+
+
+##Prerequisites
+
+- Node js needs to be installed. You can do this using the npm
+- Vuejs needs to be 
+
+```
+    npm install -g @vue/cli
+```
+
+I am also making use of a linux system. 
+
+## To Create View Project
+
+```
+vue create vuejstutorial
+
+```
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img src="./assets/build-a-bot-logo.png" alt="robot" class="logo" />
+          Build-a-Bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <!-- <HomePage /> -->
+    <RobotBuilder />
+  </main>
+</template>
+
+<script>
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HomePage,
+    RobotBuilder,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
